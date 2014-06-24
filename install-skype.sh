f9db2332988c5713edab1cc2f06a9d09a4be2313
volodja <bulaev_vladimir@mail.ru>
2014-06-24 17:54:57 +0700

    - install skype fix

--- install-skype.sh
+++ install-skype.sh
@@ -1,4 +1,4 @@
 #wget "http://download.skype.com/linux/skype-ubuntu-precise_4.3.0.37-1_i386.deb"
 wget "http://www.skype.com/go/getskype-linux-beta-ubuntu-64" -O skype.deb
 dpkg -i skype.deb
-apt-get install -f
+apt-get -y install -f
