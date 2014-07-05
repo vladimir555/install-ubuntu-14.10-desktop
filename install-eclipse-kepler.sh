05cc4b46132add5d5bf720eb5f921442b0cb81dc
volodja <bulaev_vladimir@mail.ru>
2014-07-05 07:10:43 +0700

    - eclipse fix

--- install-eclipse-kepler.sh
+++ install-eclipse-kepler.sh
@@ -1,4 +1,4 @@
 wget -c "http://eclipse.mirror.triple-it.nl/technology/epp/downloads/release/kepler/SR2/eclipse-standard-kepler-SR2-linux-gtk-x86_64.tar.gz" -O eclipse.tar.gz  &&
 tar xf eclipse.tar.gz &&
 chown -R volodja:volodja eclipse
-mv eclipse /home/volodja/eclipse-kepler &&
+mv eclipse /home/volodja/eclipse-kepler
