1d567b9ea7c65178e11aff51993d01b36aecc358
volodja <bulaev_vladimir@mail.ru>
2014-08-14 23:05:06 +0700

    - emerald config update, install until error

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
-./install-owns.sh
-./install-upgrade.sh
-./install-packages.sh
-./install-emerald.sh
-./install-skype.sh
-./install-pbzip-pigz-default.sh
-./install-zsh-default.sh
-./install-git-color.sh
-./install-eclipse-kepler.sh
-./install-configs.sh
-./install-apparmor-enforce.sh
+./install-owns.sh                 &&
+./install-upgrade.sh              &&
+./install-packages.sh             &&
+./install-emerald.sh              &&
+./install-skype.sh                &&
+./install-pbzip-pigz-default.sh   &&
+./install-zsh-default.sh          &&
+./install-git-color.sh            &&
+./install-eclipse-kepler.sh       &&
+./install-configs.sh              &&
+./install-apparmor-enforce.sh     &&
 ./remove-unnecessary-packages.sh
